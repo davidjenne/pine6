0e76ba6d1206c4e0d008698a532c6315c9a615b0
davidjenne <davidjenne@gmail.com>
2015-02-15 13:42:25 -0500

    Updates from live site 20150215

--- wp-config.php
+++ wp-config.php
@@ -7,10 +7,10 @@ if ( file_exists( dirname( __FILE__ ) . '/local-config.php' ) ) {
 	include( dirname( __FILE__ ) . '/local-config.php' );
 } else {
 	define( 'WP_LOCAL_DEV', false );
-	define( 'DB_NAME', '%%DB_NAME%%' );
-	define( 'DB_USER', '%%DB_USER%%' );
-	define( 'DB_PASSWORD', '%%DB_PASSWORD%%' );
-	define( 'DB_HOST', '%%DB_HOST%%' ); // Probably 'localhost'
+	define( 'DB_NAME', 'pinehillbeta' );
+	define( 'DB_USER', 'dsj' );
+	define( 'DB_PASSWORD', 'droopy!muffiN' );
+	define( 'DB_HOST', 'mysql.pinehill.davidjenne.com' ); // Probably 'localhost'
 }
 
 // ========================
@@ -29,14 +29,14 @@ define( 'DB_COLLATE', '' );
 // Salts, for security
 // Grab these from: https://api.wordpress.org/secret-key/1.1/salt
 // ==============================================================
-define( 'AUTH_KEY',         'put your unique phrase here' );
-define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
-define( 'LOGGED_IN_KEY',    'put your unique phrase here' );
-define( 'NONCE_KEY',        'put your unique phrase here' );
-define( 'AUTH_SALT',        'put your unique phrase here' );
-define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
-define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
-define( 'NONCE_SALT',       'put your unique phrase here' );
+define('AUTH_KEY',         '.!i!~t`l9MwdBU(!HRf5+ M7Yhu9o^l_ZaX90n`%RaN&P_blT8Wic[ kKQLG_v%M');
+define('SECURE_AUTH_KEY',  '0loz?wqCq9F#e-<]/vV-]nB@=:& Kbnm]DP(&Hn-DI6L(Mi$I7`*&y*Q|fHWWH?_');
+define('LOGGED_IN_KEY',    '4iz2|=$i6QWnr2EV+76iF~.y<>-W:WDz4OqMDOQ7LM,Rz)Y45i`A`VGLt@fDMp2c');
+define('NONCE_KEY',        '*K_Ez i3k@?8dqn*0GJ&3SLo/E?kMN0A*LN.%S$@^]*X)c8,.Sl[JT/2yEbNSgVX');
+define('AUTH_SALT',        'Z8>7p6wBKdLx.m %W!qwsH%r|CU-EAnqamdp+|1|U xLk]$IL`g8|-lWW_ukKZPh');
+define('SECURE_AUTH_SALT', 'g:$`D(vnlD.w-m8v--FZz@p;|*7r/C2$3!]$J>*[U0:#+W44`s|x@T(bT-Zo}0<H');
+define('LOGGED_IN_SALT',   'N27rB.^)VK|zen-kpf>-8M.B8.U2;Js4,*4V%2S!g49<,jw@Na^WvuP4Iti/Z%|&');
+define('NONCE_SALT',       'E(420uHY2sjY/f|/-I/nDi([zr>sN%fvu+m/0y~Tk`Px`6=*(-qka)m*OE)H5U+S');
 
 // ==============================================================
 // Table prefix
